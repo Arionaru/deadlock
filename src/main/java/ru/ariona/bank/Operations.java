d721b3d3db2b6016abf3ac4d06d438255ebb2769
СапоГ <katenochekspb@mail.ru>
2019-07-21 12:37:42 +0300

    first

--- src/main/java/ru/ariona/bank/Operations.java
+++ src/main/java/ru/ariona/bank/Operations.java
@@ -1,31 +1,26 @@
 package ru.ariona.bank;
 
-import javax.naming.InsufficientResourcesException;
 import java.util.logging.Logger;
 
 public class Operations {
 
     private static Logger log = Logger.getLogger(Operations.class.getName());
 
-    public static void main(String[] args) throws InsufficientResourcesException, InterruptedException {
+    public static void main(String[] args){
         final Account a = new Account(1000);
         final Account b = new Account(2000);
 
-        new Thread(new Runnable() {
-            public void run() {
-                try {
+        new Thread(() -> {
                     transfer(a,b,500);
-                } catch (InsufficientResourcesException | InterruptedException e) {
-                    e.printStackTrace();
-                }
+
             }
-        }).start();
+        ).start();
         transfer(b,a,300);
     }
 
-    static void transfer(Account acc1, Account acc2, int amount) throws InsufficientResourcesException, InterruptedException {
+    private static void transfer(Account acc1, Account acc2, int amount) {
         if (acc1.getBalance() < amount) {
-            throw new InsufficientResourcesException();
+            throw new RuntimeException("Недостаточно средств!");
         }
 
         if (acc1.getBalance() > acc2.getBalance()) {
